1dc07e19c6500193ef6afbc5276c7bfef21e7fad
evgeniitsvetkov <evgeniitsvetkov@yandex.ru>
2020-05-19 20:49:09 +0300

    Add admin classes

--- vacancies/admin.py
+++ vacancies/admin.py
@@ -1,3 +1,20 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from vacancies.models import Vacancy, Company, Application
+
+
+class VacancyAdmin(admin.ModelAdmin):
+    pass
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+class ApplicationAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Vacancy, VacancyAdmin)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Application, ApplicationAdmin)
